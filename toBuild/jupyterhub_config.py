400db8c103d17d438aac30cd0ededc8c29e04bbf
Enzhine <52751569+Enzhine@users.noreply.github.com>
2022-12-19 20:16:28 +0300

    Some comments

--- toBuild/jupyterhub_config.py
+++ toBuild/jupyterhub_config.py
@@ -1,11 +1,15 @@
 import pwd, subprocess
 c = get_config()
+# Setting better authenticator
 c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
+# Adding admin user
 c.Authenticator.admin_users = {"admin"}
+# Adding spawner hook to create user jupyter workspace
 def hook(sp):
     name = sp.user.name
     try:
         pwd.getpwnam(name)
     except KeyError:
         subprocess.check_call(['useradd', '-ms', '/bin/bash', name])
+# Setting hook
 c.Spawner.pre_spawn_hook = hook
